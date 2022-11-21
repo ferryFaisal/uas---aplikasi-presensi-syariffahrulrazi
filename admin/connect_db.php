982ee19af3c5fb25d65bb6fd2b8c24184bf29733
syariffahrulrazi <102273049+syariffahrulrazi@users.noreply.github.com>
2022-11-22 00:00:27 +0700

    UAS
    File Uas WEBPRO

---
+++ admin/connect_db.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$dbname = "uas";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $passwordDB, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+//  echo "Connected successfully using Mysqli Procedural";
